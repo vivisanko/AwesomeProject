81583bd797105b0ad6fae6fa45fea3938fdd3562
Victoria Sanko <sanko.victoria@gmail.com>
2019-11-09 00:35:38 +0300

    Updates

---
+++ precache-manifest.0ce511d238d8efef3cd9eb5e252e83ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9807634fa54a224d7ef5abd52910f2ba",
+    "url": "/AwesomeProject/index.html"
+  },
+  {
+    "revision": "04c63723cb392cbe3af2",
+    "url": "/AwesomeProject/static/css/main.d5ef0616.chunk.css"
+  },
+  {
+    "revision": "bba0bce5a58bbadb5d64",
+    "url": "/AwesomeProject/static/js/2.afe8dee0.chunk.js"
+  },
+  {
+    "revision": "04c63723cb392cbe3af2",
+    "url": "/AwesomeProject/static/js/main.53d2ea13.chunk.js"
+  },
+  {
+    "revision": "484fd46dab69ac6585ab",
+    "url": "/AwesomeProject/static/js/runtime-main.1b7b68ac.js"
+  },
+  {
+    "revision": "527a4343eb6756a236a82d2cda539930",
+    "url": "/AwesomeProject/static/media/avocado.527a4343.svg"
+  },
+  {
+    "revision": "99c7a9daa00ad9af48d50564f0a9c962",
+    "url": "/AwesomeProject/static/media/tomato.99c7a9da.svg"
+  }
+]);
